04f0669f5770e3c768abf3d681c483ba059df54e
niconielsen24 <niconielsen.pc@gmail.com>
2023-06-08 15:22:44 -0300

    test_anim

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -1,5 +1,4 @@
-	//.include "graph_funs.s"
-	.include "object_graph_funs.s"
+	.include "graph_funs.s"
 	.equ SCREEN_WIDTH,   640
 	.equ SCREEN_HEIGH,   480
 	.equ BITS_PER_PIXEL, 32
@@ -15,13 +14,28 @@ main:
 	mov x23, x0 // Guarda la dirección base del framebuffer en x20
 	//---------------- CODE HERE ------------------------------------
 
-	bl paint_sky_day						//--- paint_sky_day y paint_sky_night son mutualmente exclusivas, una pisa a la otra
+	// flai
 
-	bl paint_sun							//--- paint_sun anda junto con cualquiera de las 2, solo hay que llamarla despues (eventualmente una para la luna va a ser parecida) 
+	movz w10,#0x00ff,lsl 16
+	movk w10,#0xffff
 
-	bl fore_ground
+	bl background_paint
 
-	bl paint_casita
+	movz w10,#0x0000,lsl 16
+	movk w10,#0x0000
+
+	mov x5,240
+	mov x6,320
+	mov x8,100
+	mul x7,x8,x8
+
+	bl paint_circle
+
+	// GPIO config set
+	
+	mov x21, GPIO_BASE						// --self explanatory--
+
+	str wzr, [x21, GPIO_GPFSEL0]			// Setea gpios 0 - 9 como lectura
 
 	// GPIO read loop
 	//
@@ -32,27 +46,42 @@ loop_delay0:								// 	 }	Loop de delay
 	subs x10,x10,#1							//  /
 	b.ne loop_delay0						// /
 
-	mov x21, GPIO_BASE						// --self explanatory--
-
-	str wzr, [x21, GPIO_GPFSEL0]			// Setea gpios 0 - 9 como lectura
-
 	ldr w22, [x21,GPIO_GPLEV0]				// Lee el estado de los GPIO 0 - 31
 
-	and w22,w22,0b00000001					// aisla el pin 1, que por alguna razon es la F en mi teclado, ingles US
-	cbnz w22, paint_circ					// si se presiona la tecla el circulo cambia a negro
+	and w22,w22,0b00000010					// 0b00000010 = w, 0b00000100 = a, 0b00001000 = s, 0b00010000 = d, 0b00100000 = [SPACE_BAR]
+	cbnz w22, paint_animation				// si se presiona la tecla el circulo cambia a negro
 
-	movz x10,0xE100							// \
-	movk x10,0x05f5 ,lsl 16					//  \
-loop_delay1:								//   } Loop de delay
-	subs x10,x10,#1 						//  /
-	b.ne loop_delay1						// /
 	b leo_gpio								// si no se detecta un cambio en w22 se regresa a leo_gpio
 
-paint_circ:
-	bl paint_sky_night
-	bl paint_moon
-	bl fore_ground
-	bl paint_casita
+paint_animation:
+
+	ldr w22, [x21,GPIO_GPLEV0]				// Lee el estado de los GPIO 0 - 31
+	and w22,w22,0b00000010					// 0b00000010 = w, 0b00000100 = a, 0b00001000 = s, 0b00010000 = d, 0b00100000 = [SPACE_BAR]
+
+	cbz w22, leo_gpio
+
+	movz x9,0x0000							// \
+	movk x9,0x00ff ,lsl 16					//  \
+loop_delay_w:								// 	 }	Loop de delay
+	subs x9,x9,#1							//  /
+	b.ne loop_delay_w						// /
+
+
+	movz w10,#0x00ff,lsl 16
+	movk w10,#0xffff
+
+	bl paint_circle
+
+	add x5,x5,5
+	sub x8,x8,1
+	mul x7,x8,x8
+
+	movz w10,#0x0000,lsl 16
+	movk w10,#0x0000
+
+	bl paint_circle
+
+	b paint_animation
 
 	//---------------------------------------------------------------
 	// Infinite Loop
